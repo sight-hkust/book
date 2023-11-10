0aa7ac6b76e162f4acd44adcae57c75251a60fc2
Kristopher Lam <root@sandb0x.io>
2023-11-10 12:59:53 +0800

    footprint done

--- pages/the-footprint.tsx
+++ pages/the-footprint.tsx
@@ -11,7 +11,7 @@ function RegionProjectsGroup({ label, projects }: RegionProjectsGroupProps) {
   return (
     <div className="flex items-center my-16 text-[#482329]">
       <span className="h-10 w-10 rounded-full font-bold text-white bg-[#482329] flex items-center justify-center">{label}</span>
-      <ul className="flex flex-col ml-4">
+      <ul className="flex flex-col ml-4 max-w-[240px]">
         {projects.map((project, index) => (
           <li key={project} className="text-sm">
             {index < 9?`0${index+1}`:index+1}.&nbsp;
@@ -88,12 +88,13 @@ export default function Footprint() {
     <Layout
       prevPageHref="/timeline"
       nextPageHref="/the-footprint"
-      className="w-screen h-screen flex text-[#142a44]">
+      className="w-screen md:h-screen flex flex-col md:flex-row text-[#142a44]">
         <Head>
           <title>The Footprint</title>
         </Head>
-        <div className="w-1/2 bg-[#482329] h-screen"></div>
-        <div className="w-1/2 bg-[#fffef7] flex-col h-screen p-16">
+        <div className="w-full md:w-1/2 bg-[#482329] h-[640px] md:h-screen bg-[url('/media/footprint.png')] bg-contain bg-no-repeat">
+        </div>
+        <div className="w-full md:w-1/2 bg-[#fffef7] flex-col h-screen p-16">
           <h2 className="uppercase text-4xl font-bold text-[#B3BCB6]">the<br/><span className="text-[#482329]">footprint</span></h2>
           <section className="flex flex-col flex-wrap h-[840px]">
             {
