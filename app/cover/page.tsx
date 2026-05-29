bcd9fce48d6176b2b21e9d5b6cd19cd392c1e396
Kristopher Lam <root@sandb0x.io>
2026-05-29 12:33:30 +0800

    fix: update page titles from generic Next.js defaults

--- app/cover/page.tsx
+++ app/cover/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import ExportedImage from "next-image-export-optimizer";
+import Head from "next/head";
 import React from "react";
 import Layout from "../../components/Layout";
 
@@ -9,6 +10,9 @@ export default function Cover() {
       prevPageHref="/"
       nextPageHref="/chapters"
       className="w-full">
+      <Head>
+        <title>Cover</title>
+      </Head>
       <ExportedImage className="w-1/2 h-full mx-auto mt-32" src="/logo-chinese-vertical.svg" width={200} height={300} alt="SIGHT Name in Chinese"/>
     </Layout>
   )
